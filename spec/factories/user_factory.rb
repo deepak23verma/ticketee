b564c4e1fbdc3269efa35fd3283422b4b3b24713
Deepak Verma <deepak.23.verma@gmail.com>
2014-04-03 15:36:31 -0400

    Associate users to tickets when tickets are created

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
     name "Deepak"
-    email "deepak@example.com"
+    email "sample@example.com"
     password "password"
     password_confirmation "password"
   end
