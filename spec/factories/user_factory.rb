b865183f0f4405039938d2555ae596ad0cf7a42d
Deepak Verma <deepak.23.verma@gmail.com>
2014-04-09 13:05:34 -0400

    Add ability to edit and update users

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
+  sequence(:email) { |n| "user#{n}@example.com"}
+
   factory :user do
-    name "Deepak"
-    email "sample@example.com"
+    name "username"
+    email { generate(:email) }
     password "password"
     password_confirmation "password"
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 
 describe User do
+  it "requires an email" do
+    u = User.new(name: "steve",
+      password: "hunter2",
+      password_confirmation: "hunter2")
+
+    u.save
+    expect(u).to_not be_valid
+
+    u.email = "steve@example.com"
+    u.save
+    expect(u).to be_valid
+  end
+
   describe "passwords" do
     it "needs a password and confirmation to save" do
-      u = User.new(name: "steve")
+      u = User.new(name: "steve", email: "steve@example.com")
 
       u.save
       expect(u).to_not be_valid
@@ -20,6 +33,7 @@ describe User do
 
     it "needs password and confirmation to match" do
       u = User.create(name: "steve",
+        email: "steve@example.com",
         password: "hunter2",
         password_confirmation: "hunter")
 
